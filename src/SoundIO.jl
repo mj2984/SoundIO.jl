22b6aa0e357dc15bc92b4cb457021ec26f2baa0f
mj2984 <92405693+mj2984@users.noreply.github.com>
2026-03-02 15:07:05 -0500

    Create SoundIO.jl
    Initial Commit for SoundIO.jl

---
+++ src/SoundIO.jl
@@ -0,0 +1,13 @@
+module SoundIO
+using Libdl
+using libsoundio_jll
+const libsoundio = libsoundio_jll.libsoundio_path
+
+include("SoundIOConstants.jl")
+include("SoundIOStructs.jl")
+include("SoundIOPrettyPrintMethods.jl")
+include("SoundIOMethods.jl")
+
+export SoundIOContext, SoundIODevice, SoundIOOutStream, PlaybackState,
+       enumerate_devices!, open_outstream_direct, wait_events
+end
