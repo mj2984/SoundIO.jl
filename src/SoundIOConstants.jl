efbaaefcc843b6532d66a74eb51c6bae80740490
mj2984 <92405693+mj2984@users.noreply.github.com>
2026-03-02 15:07:37 -0500

    Create SoundIOConstants.jl
    Initial Commit for SoundIO constants.

---
+++ src/SoundIOConstants.jl
@@ -0,0 +1,13 @@
+# Little Endian (3 bytes), Big Endian
+@enum SoundIoFormat begin
+    FormatInvalid = 0
+    S8 = 1; U8 = 2
+    S16LE = 3; S16BE = 4
+    U16LE = 5; U16BE = 6
+    S24LE = 7; S24BE = 8
+    U24LE = 9; U24BE = 10
+    S32LE = 11; S32BE = 12
+    U32LE = 13; U32BE = 14
+    Float32LE = 15; Float32BE = 16
+    Float64LE = 17; Float64BE = 18
+end
