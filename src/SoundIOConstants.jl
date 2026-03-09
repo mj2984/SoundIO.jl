4dd7ba5dbd8dbff7f994e63c4b8033fc838dd766
mj2984 <92405693+mj2984@users.noreply.github.com>
2026-03-08 21:30:19 -0400

    Update SoundIOConstants.jl
    Update to SoundIO.jl for more julian api for common functions.

--- src/SoundIOConstants.jl
+++ src/SoundIOConstants.jl
@@ -1,7 +1,12 @@
-# Little Endian (3 bytes), Big Endian
 # Pre-resolving the memory address of the function to bypass lookup in the shared library's symbol table.
+const libsoundio = libsoundio_jll.libsoundio_path
+#const libsoundio = "C:\\Users\\manue\\Downloads\\libsoundio_build\\products\\libsoundio.v2.0.0.x86_64-w64-mingw32\\bin\\libsoundio.dll"
 const soundio_wait_events_ptr = Libdl.dlsym(Libdl.dlopen(libsoundio), :soundio_wait_events)
 const soundio_outstream_begin_write_ptr = Libdl.dlsym(Libdl.dlopen(libsoundio), :soundio_outstream_begin_write)
+const soundio_outstream_end_write_ptr = Libdl.dlsym(Libdl.dlopen(libsoundio), :soundio_outstream_end_write)
+const SoundIOBackendMemoryOffsetBytes = 0
+const SoundIOBackendNone = 0
+# Little Endian (3 bytes), Big Endian
 const SoundIoFormats = Dict{Symbol, Int32}(
     :Invalid        => 0 ,
     :Int8           => 1 , :UInt8          => 2 ,
@@ -10,4 +15,22 @@ const SoundIoFormats = Dict{Symbol, Int32}(
     :Int32Little    => 11, :Int32Big       => 12, :UInt32Little   => 13, :UInt32Big      => 14,
     :Float32Little  => 15, :Float32Big     => 16,
     :Float64Little  => 17, :Float64Big     => 18,
+)
+const SoundIoErrorMap = Dict{Int32, Symbol}(
+    0  => :Success,
+    1  => :OutOfMemory,
+    2  => :BackendInitializationFailed,
+    3  => :SystemResourcesUnavailable,
+    4  => :OpeningDeviceFailed,
+    5  => :DeviceNotFound,
+    6  => :InvalidParameter,
+    7  => :BackendUnavailable,
+    8  => :StreamingError, # Unrecoverable
+    9  => :IncompatibleDeviceParameters,
+    10 => :NoSuchClient,
+    11 => :IncompatibleBackend,
+    12 => :BackendDisconnected,
+    13 => :Interrupted,
+    14 => :BufferUnderflow,
+    15 => :EncodingError,
 )
