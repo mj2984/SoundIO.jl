e7a95063f6f3bf13de7b21a75dbf6887cd0a0b36
mj2984 <92405693+mj2984@users.noreply.github.com>
2026-03-09 04:12:49 -0400

    Update SoundIO.jl
    Updated SoundIO jl with native julia callbacks.

--- src/SoundIO.jl
+++ src/SoundIO.jl
@@ -4,8 +4,10 @@ include("SoundIOConstants.jl")
 include("SoundIOStructs.jl")
 include("SoundIOPrettyPrintMethods.jl")
 include("SoundIOMethods.jl")
+#include("Experimental.jl")
 export SoundIOContext, SoundIODevice, SoundIOOutStream, FrozenAudioBuffer,
        is_connected_unsafe, is_connected,open_unsafe!, open!, connect_unsafe!, connect!,
        disconnect_unsafe!, disconnect!, flush_events_unsafe!, flush_events!, wait_unsafe,
        enumerate_devices_unsafe!, enumerate_devices!, play_audio, supported_formats
+       #play_audio_threaded
 end
