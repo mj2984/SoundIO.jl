ab935077b7cb61da310d03e2d5bfcad4eaba104e
mj2984 <92405693+mj2984@users.noreply.github.com>
2026-03-09 19:22:27 -0400

    Update SoundIO_test.jl
    SoundIO Update.

--- test/SoundIO_test.jl
+++ test/SoundIO_test.jl
@@ -1,4 +1,6 @@
-using WAV
 include(raw"SoundIODemo.jl")
-sound_file = "myfile.wav"
+sound_file = raw"C:\Users\manue\Downloads\01 - Song Sohee - Dear My Lover.wav"
+#ctx = SoundIOContext()
+#connect!(ctx)
+#enumerate_devices!(ctx)
 play_music(sound_file)
