eb4671e287ba1dcf1af0bb0a4537770ff865c37b
mj2984 <92405693+mj2984@users.noreply.github.com>
2026-03-09 13:54:24 -0400

    Update SoundIO.jl
    Update to SoundIO

--- src/SoundIO.jl
+++ src/SoundIO.jl
@@ -7,10 +7,9 @@ include("SoundIOConstants.jl")
 include("SoundIOStructs.jl")
 include("SoundIOPrettyPrintMethods.jl")
 include("SoundIOMethods.jl")
-#include("Experimental.jl")
 export SoundIOContext, SoundIODevice, SoundIOOutStream, FrozenAudioBuffer,
+       SoundIOSynchronizer, AudioCallbackSynchronizer,
        is_connected_unsafe, is_connected,open_unsafe!, open!, connect_unsafe!, connect!,
        disconnect_unsafe!, disconnect!, flush_events_unsafe!, flush_events!, wait_unsafe,
        enumerate_devices_unsafe!, enumerate_devices!, play_audio, supported_formats
-       #play_audio_threaded
 end
