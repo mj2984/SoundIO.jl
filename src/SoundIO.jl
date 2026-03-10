799a108f2202115160521307740715503ad36535
mj2984 <92405693+mj2984@users.noreply.github.com>
2026-03-10 01:45:38 -0400

    Update SoundIO.jl

--- src/SoundIO.jl
+++ src/SoundIO.jl
@@ -12,5 +12,5 @@ export SoundIOContext, SoundIODevice, SoundIOOutStream, FrozenAudioBuffer,
        is_connected_unsafe, is_connected,open_unsafe!, open!, connect_unsafe!, connect!,
        disconnect_unsafe!, disconnect!, flush_events_unsafe!, flush_events!, wait_unsafe,
        enumerate_devices_unsafe!, enumerate_devices!, play_audio, supported_formats,
-       get_callbackSyncData, start!
+       start!
 end
