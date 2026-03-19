90fe3d12d9f6f3cb6bd77c98fda308c275ca5a1d
mj2984 <92405693+mj2984@users.noreply.github.com>
2026-03-19 15:51:48 -0400

    Update SoundIOMethods.jl
    Auto clearing implemented as type.

--- src/SoundIOMethods.jl
+++ src/SoundIOMethods.jl
@@ -15,54 +15,53 @@ end
     ccall(soundio_outstream_begin_write_ptr, Cint, (Ptr{Cvoid}, Ptr{Ptr{SoundIoChannelArea_C}}, Ptr{Cint}), outstream_ptr, areas_ref, frames_ref)
     return unsafe_load(areas_ref[]).ptr, Int(frames_ref[]::Cint) # Note: unsafe_load(areas_ref[]) returns a SoundIoChannelArea_C
 end
-function get_source_ptr_base(buffer::FrozenAudioBuffer{T,Channels,isatomic}) where {T,Channels,isatomic}
-    if(isatomic)
-        return Ptr{UInt8}(buffer.layout.data_ptr) + buffer.stream.current_offset_base
-    else
-        return Ptr{UInt8}(buffer.layout.data_ptr)
-    end
-end
-get_frames_to_copy(buffer::FrozenAudioBuffer{T,Channels,isatomic},actual_frames::Int) where {T,Channels,isatomic} = min(actual_frames, buffer.layout.atom_frames - buffer.stream.atomic_frame_offset)
-function frozen_audio_callback_boundary_handler!(buffer::FrozenAudioBuffer{T,Channels,isatomic}, destination_ptr::Ptr{UInt8}, frames_copied::Int, actual_frames::Int, bytes_per_frame::Int) where {T,Channels,isatomic}
+@inline get_source_ptr_base(buffer::FrozenAudioBuffer{T,Channels,isatomic,isclearing}) where {T,Channels,isatomic,isclearing} = isatomic ? Ptr{UInt8}(buffer.layout.data_ptr) + buffer.stream.current_offset_base : Ptr{UInt8}(buffer.layout.data_ptr)
+@inline get_frames_to_copy(buffer::FrozenAudioBuffer{T,Channels,isatomic,isclearing},actual_frames::Int) where {T,Channels,isatomic,isclearing} = min(actual_frames, buffer.layout.atom_frames - buffer.stream.atomic_frame_offset)
+function frozen_audio_callback_boundary_handler!(buffer::FrozenAudioBuffer{T,Channels,isatomic,isclearing}, destination_ptr::Ptr{UInt8}, frames_copied::Int, actual_frames::Int, bytes_per_frame::Int) where {T,Channels,isatomic,isclearing} # Handle Silence / End-of-Buffer-Atom
     layout,stream = buffer.layout, buffer.stream
-    status::Int8 = @atomic stream.status
+    exchange::FrozenAudioExchange = @atomic stream.exchange
     pending_frames = actual_frames - frames_copied
     starting_ptr = destination_ptr + (frames_copied * bytes_per_frame)
     pending_bytes = pending_frames * bytes_per_frame
-    if status == CallbackJuliaDone
-        if(isatomic)
+    elapsed_atoms::Int = isatomic ? exchange.elapsed_atoms + 1 : 0
+    stream.atomic_frame_offset = pending_frames
+    return_status::Int8 = exchange.status
+    if return_status == CallbackJuliaDone
+        if isatomic
             atom_bytes = layout.atom_frames * bytes_per_frame # assert pending_bytes < atom_bytes (but this should be done outside)
-            total_bytes = layout.total_atoms * atom_bytes
-            next_offset_base = (stream.current_offset_base + atom_bytes) % total_bytes
+            next_offset_base = (stream.current_offset_base + atom_bytes) % (layout.total_atoms * atom_bytes) # Wrap back to 0 at end of loop.
             stream.current_offset_base = next_offset_base
-            @atomic stream.elapsed_frames = div(next_offset_base, bytes_per_frame) + pending_frames
-        else
-            @atomic stream.elapsed_frames = pending_frames
         end
-        stream.atomic_frame_offset = pending_frames
         next_atom_ptr = get_source_ptr_base(buffer)
         unsafe_copyto!(starting_ptr, next_atom_ptr, pending_bytes)
+        if isclearing
+            ccall(:memset, Ptr{Cvoid}, (Ptr{Cvoid}, Cint, Csize_t), next_atom_ptr, 0, pending_bytes)
+        end
     else
+        return_status = CallbackStopped
         ccall(:memset, Ptr{Cvoid}, (Ptr{Cvoid}, Cint, Csize_t), starting_ptr, 0, pending_bytes)
-        @atomic stream.status = CallbackStopped
     end
+    @atomic stream.exchange = FrozenAudioExchange(pending_frames, elapsed_atoms, return_status)
     ccall(:uv_async_send, Cint, (Ptr{Cvoid},), stream.notify_handle.handle)
 end
 function frozen_audio_callback(outstream_ptr::Ptr{SoundIoOutStream_C},frames_min::Cint,frames_max::Cint,::Type{BufType}) where {BufType<:FrozenAudioBuffer}
-    buffer::BufType = get_audio_buffer(outstream_ptr,BufType)# Recover the Julia Object
-    T,Channels = BufType.parameters
+    buffer::BufType = get_audio_buffer(outstream_ptr,BufType)
+    T,Channels,isatomic,isclearing = BufType.parameters
     bytes_per_frame::Int = Channels * sizeof(T)
     buffer_ptr, actual_frames::Int = negotiate_callback_buffer_space(outstream_ptr,frames_max)
     source_ptr_base = get_source_ptr_base(buffer)
     destination_ptr = Base.unsafe_convert(Ptr{UInt8},buffer_ptr) # Base.unsafe_convert(Ptr{T},unsafe_load(buffer_ref.areas[]).ptr)
     frames_to_copy::Int = get_frames_to_copy(buffer,actual_frames)
-    if frames_to_copy > 0 # (stream.status == CallbackJuliaDone) && # This check has been removed since we are not looking to halt memory playback during runtime.
+    if frames_to_copy > 0
         source_ptr = source_ptr_base + (buffer.stream.atomic_frame_offset * bytes_per_frame) # layout.data_ptr + (stream.current_frame * Channels)
         data_bytes_to_copy = frames_to_copy * bytes_per_frame #data_to_copy = frames_to_copy * Channels # In Units of T
         unsafe_copyto!(destination_ptr,source_ptr,data_bytes_to_copy) #unsafe_copyto!(destination_ptr, source_ptr, data_to_copy)
+        if isclearing
+            ccall(:memset, Ptr{Cvoid}, (Ptr{Cvoid}, Cint, Csize_t), source_ptr, 0, data_bytes_to_copy)
+        end
         buffer.stream.atomic_frame_offset += frames_to_copy
     end
-    if frames_to_copy < actual_frames # Handle Silence / End-of-Buffer
+    if frames_to_copy < actual_frames
         frozen_audio_callback_boundary_handler!(buffer,destination_ptr,frames_to_copy,actual_frames,bytes_per_frame)
     end
     ccall(soundio_outstream_end_write_ptr, Cint, (Ptr{Cvoid},), outstream_ptr) # Commit to Hardware
@@ -218,7 +217,7 @@ function open_sound_stream(device::SoundIODevice, buffer::T, sample_rate::Intege
     return open_sound_stream(device, buffer, sample_rate, SoundIoFormats[format], latency_seconds)
 end
 function Base.open(device::SoundIODevice, bufferspec, channels::Integer, sample_rate::Integer, format::Union{Symbol,Int32}, latency_seconds::Float64 = 1.0)
-    if(bufferspec isa Tuple{Ptr{T},Integer,Integer} where T)
+    if bufferspec isa Tuple{Ptr, Integer, Integer, Bool}
         buffer = FrozenAudioBuffer(bufferspec...,channels)
     else
         buffer = AudioCallbackSynchronizer(bufferspec,channels)
@@ -233,7 +232,8 @@ function reopen!(stream::SoundIOOutStream)
     return nothing
 end
 function update_callback_status_message(sync::FrozenAudioBuffer,status::Int8)
-    @atomic sync.stream.status = status
+    exchange = @atomic sync.stream.exchange
+    @atomic sync.stream.exchange = FrozenAudioExchange(exchange.elapsed_frame_bytes,exchange.elapsed_atoms,status)
     return nothing
 end
 function update_callback_status_message(sync::AudioCallbackSynchronizer,status::Int8)
@@ -276,8 +276,10 @@ function supported_formats(device::SoundIODevice)
 end
 @inline Base.wait(stream::FrozenAudioStream) = wait(stream.notify_handle)
 @inline Base.wait(sync::AudioCallbackSynchronizer) = wait(sync.notify_handle)
+@inline get_exchange(stream::FrozenAudioStream) = @atomic stream.exchange
 @inline function Base.close(stream::FrozenAudioStream)
-    @atomic stream.status = CallbackStopped
+    exchange = @atomic stream.exchange
+    @atomic stream.exchange = FrozenAudioExchange(exchange.elapsed_frame_bytes,exchange.elapsed_atoms,CallbackStopped)
     close(stream.notify_handle)
 end
 @inline function acquire_sound_buffer_ptr(sync::AudioCallbackSynchronizer{T, Channels}) where {T, Channels}
