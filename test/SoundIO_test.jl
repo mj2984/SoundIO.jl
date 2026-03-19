6637850c014da2e484ff9017b2117f0af0fe8b2b
mj2984 <92405693+mj2984@users.noreply.github.com>
2026-03-18 21:22:46 -0400

    Update SoundIO_test.jl
    SoundIO Initial Stable commit.

--- test/SoundIO_test.jl
+++ test/SoundIO_test.jl
@@ -1,6 +1,9 @@
 include(raw"SoundIODemo.jl")
 sound_file = raw"sound_file.wav"
-#ctx = SoundIOContext()
-#connect!(ctx)
-#enumerate_devices!(ctx)
-play_music(sound_file)
+ctx = SoundIOContext()
+connect!(ctx)
+enumerate_devices!(ctx)
+audio_device = filter(d -> (!d.is_input) & (d.is_raw), ctx.devices)[1]
+println("🎶 Playing: $sound_file")
+play_music(sound_file,audio_device)
+println("Finished!")
