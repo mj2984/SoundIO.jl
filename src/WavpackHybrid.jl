10465ec5df09f01fa9ac0ca2f195399cbc9ed265
mj2984 <92405693+mj2984@users.noreply.github.com>
2026-03-26 10:08:03 -0400

    Update WavpackHybrid.jl
    Update with Hybrid noise shaping

--- src/WavpackHybrid.jl
+++ src/WavpackHybrid.jl
@@ -1,6 +1,6 @@
-module WavPackHybridNoiseToy
+module WavPackHybridBitrateToy
 
-export hybrid_encode, hybrid_decode, test_hybrid_noise_codec
+export encode, decode, test_codec
 
 # -------------------------
 # Constants
@@ -20,13 +20,13 @@ end
 LMS() = LMS(zeros(Int32, MAX_TAPS), zeros(Int32, MAX_TAPS))
 
 @inline function clamp16(x::Int32)
-    return Int16(clamp(x, typemin(Int16), typemax(Int16)))
+    Int16(clamp(x, typemin(Int16), typemax(Int16)))
 end
 
 @inline function get_delta(sample::Int32)
     mag = abs(sample)
     idx = min(15, (32 - leading_zeros(UInt32(mag))) >> 1)
-    return UPDATE_TABLE[idx + 1]
+    UPDATE_TABLE[idx+1]
 end
 
 function lms_predict(s::LMS)
@@ -58,8 +58,8 @@ end
 # Residual splitting
 # -------------------------
 function compute_shift(err::Int32)
-    idx = min(abs(err),15)+1
-    return HYBRID_SHIFT_TABLE[idx]
+    idx = min(abs(err), 15) + 1
+    HYBRID_SHIFT_TABLE[idx]
 end
 
 function split_residual(err::Int32, shift::Int)
@@ -71,7 +71,7 @@ end
 
 function compute_rice_k(x::Int32)
     mag = max(abs(x),1)
-    return clamp(fld(31 - leading_zeros(UInt32(mag)),2), 0, 15)
+    clamp(fld(31 - leading_zeros(UInt32(mag)),2), 0, 15)
 end
 
 # -------------------------
@@ -93,34 +93,34 @@ function write_bits!(bw::BitWriter, value::UInt32, nbits::Int)
         bw.bits_filled += take
         nbits -= take
         if bw.bits_filled == 8
-            push!(bw.data,bw.buffer)
-            bw.buffer=0x00
-            bw.bits_filled=0
+            push!(bw.data, bw.buffer)
+            bw.buffer = 0x00
+            bw.bits_filled = 0
         end
     end
 end
 
 function flush_bits!(bw::BitWriter)
-    if bw.bits_filled>0
-        push!(bw.data,bw.buffer)
-        bw.buffer=0x00
-        bw.bits_filled=0
+    if bw.bits_filled > 0
+        push!(bw.data, bw.buffer)
+        bw.buffer = 0x00
+        bw.bits_filled = 0
     end
 end
 
-function write_unary!(bw::BitWriter,value::Int)
+function write_unary!(bw::BitWriter, value::Int)
     for _ in 1:value
-        write_bits!(bw,UInt32(1),1)
+        write_bits!(bw, UInt32(1), 1)
     end
-    write_bits!(bw,UInt32(0),1)
+    write_bits!(bw, UInt32(0), 1)
 end
 
-function rice_encode(bw::BitWriter,x::Int32,k::Int)
+function rice_encode(bw::BitWriter, x::Int32, k::Int)
     u = UInt32(x<0 ? (-x<<1)-1 : x<<1)
     q = u >> k
     r = u & ((1<<k)-1)
-    write_unary!(bw,Int(q))
-    write_bits!(bw,UInt32(r),k)
+    write_unary!(bw, Int(q))
+    write_bits!(bw, UInt32(r), k)
 end
 
 mutable struct BitReader
@@ -137,9 +137,9 @@ function read_bits!(br::BitReader, nbits::Int)
         if br.bits_left==0
             br.buffer = br.data[br.pos]
             br.pos += 1
-            br.bits_left=8
+            br.bits_left = 8
         end
-        take = min(nbits,br.bits_left)
+        take = min(nbits, br.bits_left)
         result <<= take
         shift = br.bits_left - take
         result |= UInt32((br.buffer >> shift) & ((1<<take)-1))
@@ -158,82 +158,85 @@ function read_unary!(br::BitReader)
 end
 
 function rice_decode(br::BitReader,k::Int)
-    q=read_unary!(br)
-    r=read_bits!(br,k)
-    u=(q<<k)|r
-    return Int32((u&1)!=0 ? -((u+1)>>1) : (u>>1))
+    q = read_unary!(br)
+    r = read_bits!(br,k)
+    u = (q<<k)|r
+    Int32((u&1)!=0 ? -((u+1)>>1) : (u>>1))
 end
 
 # -------------------------
-# Noise Shaper
+# Hybrid Encoder (lossy + correction)
 # -------------------------
-mutable struct Shaper
-    feedback::Int32
-end
-Shaper() = Shaper(0)
-
-# -------------------------
-# Hybrid Lossless + Noise Shaping Encoder
-# -------------------------
-function hybrid_encode(X::Matrix{Int16})
+function encode(X::Matrix{Int16}; blocksize::Int=1024, target_bps::Float64=8.0)
     N,C = size(X)
     @assert C==2 "Only stereo supported"
-    lms = [LMS(),LMS()]
-    shapers = [Shaper(),Shaper()]
+
+    lms = [LMS(), LMS()]
     lossy_bw = BitWriter()
     corr_bw  = BitWriter()
 
-    for n in 1:N
-        L = Int32(X[n,1])
-        R = Int32(X[n,2])
+    idx = 1
+    while idx <= N
+        blk_end = min(idx+blocksize-1, N)
 
-        # Joint stereo
-        mid  = L - (R >> 1)
-        side = R
+        for n in idx:blk_end
+            L = Int32(X[n,1])
+            R = Int32(X[n,2])
 
-        predM = lms_predict(lms[1])
-        predS = lms_predict(lms[2])
-        errM = mid - predM
-        errS = side - predS
+            # --- normal stereo residuals ---
+            predL = lms_predict(lms[1])
+            predR = lms_predict(lms[2])
+            errL = L - predL
+            errR = R - predR
+            shiftL = compute_shift(errL)
+            shiftR = compute_shift(errR)
+            qL,cL = split_residual(errL, shiftL)
+            qR,cR = split_residual(errR, shiftR)
 
-        # --- Noise shaping feedback ---
-        shapedM = errM + (shapers[1].feedback >> 3)
-        shapedS = errS + (shapers[2].feedback >> 3)
+            # --- joint stereo ---
+            mid = L - (R>>1)
+            side = R
+            predM = lms_predict(lms[1])
+            predS = lms_predict(lms[2])
+            errM = mid - predM
+            errS = side - predS
+            shiftM = compute_shift(errM)
+            shiftS = compute_shift(errS)
+            qM,cM = split_residual(errM, shiftM)
+            qS,cS = split_residual(errS, shiftS)
 
-        shiftM = compute_shift(shapedM)
-        shiftS = compute_shift(shapedS)
-        qM,cM = split_residual(shapedM,shiftM)
-        qS,cS = split_residual(shapedS,shiftS)
+            sum_normal = abs(qL)+abs(cL)+abs(qR)+abs(cR)
+            sum_joint  = abs(qM)+abs(cM)+abs(qS)+abs(cS)
 
-        # Update feedback for shaping
-        shapers[1].feedback = shapedM - ((qM<<shiftM)+cM)
-        shapers[2].feedback = shapedS - ((qS<<shiftS)+cS)
+            use_joint = sum_joint < sum_normal ? 1 : 0
+            write_bits!(lossy_bw, UInt32(use_joint), 1)
 
-        # --- Write lossy stream (q only) ---
-        write_bits!(lossy_bw,UInt32(shiftM),4)
-        write_bits!(lossy_bw,UInt32(shiftS),4)
-        kM = compute_rice_k(qM)
-        kS = compute_rice_k(qS)
-        write_bits!(lossy_bw,UInt32(kM),4)
-        write_bits!(lossy_bw,UInt32(kS),4)
-        rice_encode(lossy_bw,qM,kM)
-        rice_encode(lossy_bw,qS,kS)
+            if use_joint==1
+                channels = [(qM,cM,shiftM,lms[1]), (qS,cS,shiftS,lms[2])]
+            else
+                channels = [(qL,cL,shiftL,lms[1]), (qR,cR,shiftR,lms[2])]
+            end
 
-        # --- Write correction stream (c only) ---
-        write_bits!(corr_bw,UInt32(shiftM),4)
-        write_bits!(corr_bw,UInt32(shiftS),4)
-        kMc = compute_rice_k(cM)
-        kSc = compute_rice_k(cS)
-        write_bits!(corr_bw,UInt32(kMc),4)
-        write_bits!(corr_bw,UInt32(kSc),4)
-        rice_encode(corr_bw,cM,kMc)
-        rice_encode(corr_bw,cS,kSc)
+            for (q,c,shift,s) in channels
+                write_bits!(lossy_bw, UInt32(shift), 4)
+                kq = compute_rice_k(q)
+                kc = compute_rice_k(c)
+                write_bits!(lossy_bw, UInt32(kq), 4)
+                write_bits!(lossy_bw, UInt32(kc), 4)
+                rice_encode(lossy_bw, q, kq)
+                rice_encode(lossy_bw, c, kc)
 
-        # Update LMS with full original residuals
-        lms_update!(lms[1], errM)
-        lms_update!(lms[2], errS)
-        lms[1].history[1] += errM
-        lms[2].history[1] += errS
+                # --- correction stream carries only residual not in lossy quantization ---
+                corr_val = Int32(c)  # c is extra bits needed for perfect reconstruction
+                rice_encode(corr_bw, corr_val, kc)
+
+                # LMS update
+                recon_err = (q<<shift) + c
+                lms_update!(s, recon_err)
+                s.history[1] += recon_err
+            end
+        end
+        idx += blocksize
     end
 
     flush_bits!(lossy_bw)
@@ -242,56 +245,65 @@ function hybrid_encode(X::Matrix{Int16})
 end
 
 # -------------------------
-# Hybrid Lossless + Noise Shaping Decoder
+# Hybrid Decoder
 # -------------------------
-function hybrid_decode(lossy::Vector{UInt8}, corr::Vector{UInt8}, N::Int)
+function decode(lossy::Vector{UInt8}, corr::Vector{UInt8}, N::Int; blocksize::Int=1024)
+    C=2
     lms = [LMS(),LMS()]
+    Xrec = zeros(Int16,N,C)
+
     lossy_br = BitReader(lossy)
     corr_br  = BitReader(corr)
-    Xrec = zeros(Int16,N,2)
+    idx = 1
 
-    for n in 1:N
-        shiftM = Int(read_bits!(lossy_br,4))
-        shiftS = Int(read_bits!(lossy_br,4))
-        kM = Int(read_bits!(lossy_br,4))
-        kS = Int(read_bits!(lossy_br,4))
-        qM = rice_decode(lossy_br,kM)
-        qS = rice_decode(lossy_br,kS)
+    while idx <= N
+        blk_end = min(idx+blocksize-1, N)
+        for n in idx:blk_end
+            use_joint = read_bits!(lossy_br,1)
+            decoded = zeros(Int32,2)
 
-        shiftMc = Int(read_bits!(corr_br,4))
-        shiftSc = Int(read_bits!(corr_br,4))
-        kMc = Int(read_bits!(corr_br,4))
-        kSc = Int(read_bits!(corr_br,4))
-        cM = rice_decode(corr_br,kMc)
-        cS = rice_decode(corr_br,kSc)
+            for ch in 1:2
+                s = lms[ch]
+                shift = Int(read_bits!(lossy_br,4))
+                kq = Int(read_bits!(lossy_br,4))
+                kc = Int(read_bits!(lossy_br,4))
+                q = rice_decode(lossy_br, kq)
+                c_lossy = rice_decode(lossy_br, kc)
+                corr_val = rice_decode(corr_br, kc)  # correction
+                err = (q<<shift) + corr_val  # use correction only, not c_lossy
+                sample = lms_predict(s) + err
+                decoded[ch] = sample
+                lms_update!(s, err)
+                s.history[1] += err
+            end
 
-        errM = (qM << shiftM) + cM
-        errS = (qS << shiftS) + cS
+            if use_joint==1
+                mid = decoded[1]
+                side = decoded[2]
+                L = mid + (side>>1)
+                R = side
+            else
+                L = decoded[1]
+                R = decoded[2]
+            end
 
-        mid  = lms_predict(lms[1]) + errM
-        side = lms_predict(lms[2]) + errS
-        L = mid + (side >> 1)
-        R = side
-
-        Xrec[n,1] = clamp16(L)
-        Xrec[n,2] = clamp16(R)
-
-        lms_update!(lms[1], errM)
-        lms_update!(lms[2], errS)
-        lms[1].history[1] += errM
-        lms[2].history[1] += errS
+            Xrec[n,1] = clamp16(L)
+            Xrec[n,2] = clamp16(R)
+        end
+        idx += blocksize
     end
+
     return Xrec
 end
 
 # -------------------------
 # Test
 # -------------------------
-function test_hybrid_noise_codec()
-    println("Running WavPackHybridNoiseToy test...")
+function test_codec()
+    println("Running WavPackHybridBitrateToy test...")
     X = rand(Int16,5000,2)
-    lossy, corr = hybrid_encode(X)
-    Xrec = hybrid_decode(lossy,corr,5000)
+    lossy, corr = encode(X)
+    Xrec = decode(lossy, corr, 5000)
     if X==Xrec
         println("✅ Perfect reconstruction")
     else
@@ -302,8 +314,5 @@ end
 
 end
 
-# -------------------------
-# Run test
-# -------------------------
-using .WavPackHybridNoiseToy
-WavPackHybridNoiseToy.test_hybrid_noise_codec()
+using .WavPackHybridBitrateToy
+WavPackHybridBitrateToy.test_codec()
