06d6281ed0948c3f88928a677ce6becd0cbd3c88
mj2984 <92405693+mj2984@users.noreply.github.com>
2026-03-29 15:29:00 -0400

    Update SoundIO_test.jl
    Context hiding initial commit.

--- test/SoundIO_test.jl
+++ test/SoundIO_test.jl
@@ -1,8 +1,7 @@
 include(raw"SoundIODemo.jl")
 sound_file = raw"sound_file.wav"
-ctx = SoundIOContext()
-connect!(ctx)
-enumerate_devices!(ctx)
+enumerate_sound_devices!()
+audio_device = filter(d -> (!d.is_input) & (d.is_raw), list_sound_devices())[1]
 audio_device = filter(d -> (!d.is_input) & (d.is_raw), ctx.devices)[1]
 println("🎶 Playing: $sound_file")
 play_music(sound_file,audio_device)
