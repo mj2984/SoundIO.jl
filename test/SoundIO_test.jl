ede50ba1db92eac8c74af51eeca18477537eca2b
mj2984 <92405693+mj2984@users.noreply.github.com>
2026-03-29 15:29:11 -0400

    Update SoundIO_test.jl

--- test/SoundIO_test.jl
+++ test/SoundIO_test.jl
@@ -2,7 +2,6 @@ include(raw"SoundIODemo.jl")
 sound_file = raw"sound_file.wav"
 enumerate_sound_devices!()
 audio_device = filter(d -> (!d.is_input) & (d.is_raw), list_sound_devices())[1]
-audio_device = filter(d -> (!d.is_input) & (d.is_raw), ctx.devices)[1]
 println("🎶 Playing: $sound_file")
 play_music(sound_file,audio_device)
 println("Finished!")
