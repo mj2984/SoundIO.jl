ba2bb361d2e0275df62957614c19665bf1fc4e6c
mj2984 <92405693+mj2984@users.noreply.github.com>
2026-04-07 11:32:33 -0400

    Update SoundIO_test.jl

--- test/SoundIO_test.jl
+++ test/SoundIO_test.jl
@@ -1,7 +1,7 @@
-include(raw"SoundIODemo.jl")
 sound_file = raw"sound_file.wav"
 enumerate_sound_devices!()
-audio_device = filter(d -> (!d.is_input) & (d.is_raw), list_sound_devices())[1]
+audio_device::SoundIODevice = filter(d -> (!d.is_input) & (d.is_raw), list_sound_devices())[1]
 println("🎶 Playing: $sound_file")
-play_music(sound_file,audio_device)
+audio_data::SampleArray = audioread(sound_file,false) # SampleArray contains information about sample rate.
+play_audio(audio_device,audio_data)
 println("Finished!")
