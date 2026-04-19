46bb521ee9a5e976951fe39a63a6ffc49ab98f79
mj2984 <92405693+mj2984@users.noreply.github.com>
2026-04-19 06:54:38 -0400

    Update SoundIOBaseCallbacks.jl
    Updated with support for latest DomainArrays.

--- src/SoundIOBaseCallbacks.jl
+++ src/SoundIOBaseCallbacks.jl
@@ -65,18 +65,18 @@ function realtime_audio_callback(outstream_ptr::Ptr{StreamBaseType}, frames_min:
     commit_callback_buffer!(outstream_ptr)
     return nothing
 end
-function open_sound_stream(device_configuration::SoundIODeviceConfiguration{StreamBaseType,Access,Cint,Cint}, bufferspec::Tuple{Ptr{T}, Tuple{Integer, Integer}, Bool}, preserve::Any, latency_seconds::Float64 = 1.0) where {StreamBaseType,Access,T<:Sample}
+function open_sound_stream(device_configuration::SoundIODeviceConfiguration{StreamBaseType,Mode,Cint,Cint}, bufferspec::Tuple{Ptr{T}, Tuple{Integer, Integer}, Bool}, preserve::Any, latency_seconds::Float64 = 1.0) where {StreamBaseType,Mode,T<:Sample}
     buffer = FrozenAudioBuffer(bufferspec...)
     callback = make_audio_callback(StreamBaseType,typeof(buffer),frozen_audio_callback)
     return open_sound_stream(device_configuration, buffer, callback, preserve, latency_seconds)
 end
-function open_sound_stream(device_configuration::SoundIODeviceConfiguration{StreamBaseType,Access,Cint,fmt_type}, bufferspec::Type{<:Sample}, preserve::Any, latency_seconds::Float64 = 1.0) where {StreamBaseType,Access,fmt_type}
+function open_sound_stream(device_configuration::SoundIODeviceConfiguration{StreamBaseType,Mode,Cint,Cint}, bufferspec::Type{<:Sample}, preserve::Any, latency_seconds::Float64 = 1.0) where {StreamBaseType,Mode}
     buffer = AudioCallbackSynchronizer(bufferspec)
-    device_configuration_resolved = fmt_type == Nothing ? SoundIODeviceConfiguration(device_configuration.device,device_configuration.layout,device_configuration.sample_rate,get_destination_format(bufferspec)) : device_configuration
     callback = make_audio_callback(StreamBaseType,typeof(buffer),realtime_audio_callback)
-    return open_sound_stream(device_configuration_resolved, buffer, callback, preserve, latency_seconds)
+    return open_sound_stream(device_configuration, buffer, callback, preserve, latency_seconds)
 end
-open_sound_stream(device_configuration::SoundIODeviceConfiguration, bufferspec::Tuple{DataType,Integer}, preserve::Any, latency_seconds::Float64 = 1.0) = open_sound_stream(device_configuration,Sample{bufferspec[2],bufferspec[1]},preserve,latency_seconds)
+open_sound_stream(device_configuration::Tuple{SoundIODevice,SoundIoChannelLayout,Union{Symbol,Int32}}, sample_rate::Integer, bufferspec::Tuple{DataType,Integer}, preserve::Any, latency_seconds::Float64 = 1.0) = open_sound_stream(device_configuration,sample_rate,Sample{bufferspec[2],bufferspec[1]},preserve,latency_seconds)
+open_sound_stream(device_configuration::Tuple{SoundIODevice,SoundIoChannelLayout}, sample_rate::Integer, bufferspec, preserve::Any, latency_seconds::Float64 = 1.0) = open_sound_stream((device_configuration[1],device_configuration[2],get_destination_format(bufferspec)),sample_rate,bufferspec,preserve,latency_seconds)
 is_pointer_safe(A::DenseArray) = true
 is_pointer_safe(A::SubArray) = Base.iscontiguous(A)
 is_pointer_safe(A::Base.ReinterpretArray{T,N,S,P}) where {T,N,S,P} = isbitstype(T) && is_pointer_safe(parent(A))
@@ -111,12 +111,12 @@ end
 end
 function resolve_frozen_buffer_device_configuration(device_configuration::SoundIODeviceConfiguration{StreamBaseType,Access,fmt_type,sample_rate_type},audio_data::ArrayType) where {StreamBaseType,Access,fmt_type,sample_rate_type,T,N,ArrayType <: AbstractArray{T,N}}
     if sample_rate_type == Nothing && ArrayType <: DomainArray
-        sample_rate = (T <: Sample) ? audio_data.rate[1] : audio_data.rate[2]
+        sample_rate = (T <: Sample) ? interpret_rate(rate(audio_data,1)) : interpret_rate(rate(audio_data,2))
         format = fmt_type == Nothing ? get_destination_format(T) : device_configuration.format
         return SoundIODeviceConfiguration(device_configuration.device,device_configuration.layout,sample_rate,format)
     elseif sample_rate_type == Cint
         if ArrayType <: DomainArray
-            audio_rate = (T <: Sample) ? audio_data.rate[1] : audio_data.rate[2]
+            audio_rate = (T <: Sample) ? interpret_rate(rate(audio_data,1)) : interpret_rate(rate(audio_data,2))
             if device_configuration.sample_rate != audio_rate
                 error("Ambiguous sample rate arguments")
             end
