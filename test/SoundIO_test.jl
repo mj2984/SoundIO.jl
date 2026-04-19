c6b758edc1e917ca77309a57136cdab745760606
mj2984 <92405693+mj2984@users.noreply.github.com>
2026-04-18 21:35:21 -0400

    Update SoundIO_test.jl
    Significant API Changes.

--- test/SoundIO_test.jl
+++ test/SoundIO_test.jl
@@ -1,8 +1,10 @@
 sound_file = raw"sound_file.wav"
 enumerate_devices!(sounddevices)
 audio_device::SoundIODevice = list_devices(sounddevices).outputs[1]
+layout = audio_device.layouts[1]
+audio_device_configuration = SoundIODeviceConfiguration(audio_device,layout)
 println("🎶 Playing: $sound_file")
 audio_data::DomainArray = audioread(sound_file,false) # SampleArray contains information about sample rate.
 audio_view = view(audio_data,0:10) # range provided in domain axis. Here it provides the first 10 seconds.
-play_audio(audio_device,audio_view)
+play_audio(audio_device_configuration,audio_data)
 println("Finished!")
